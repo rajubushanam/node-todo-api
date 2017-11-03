7b24991df908bcdffb2769646100e6333c951d30
Sri Raju Bushanam Vanteddu <srirajubushanam@gmail.com>
2017-11-02 23:32:05 -0400

    Added post method

--- server/server.js
+++ server/server.js
@@ -1,44 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
 
-var newUser = new User({
-  userName: 'rajubushanam',
-  email: ' srirajubushanam@gmail.com '
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-var newUser1 = new User({
-  userName: 'rajubushanam123'
-});
 
-newUser.save().then((doc) => {
-  console.log('Saved', doc);
-}, (e) => {
-  console.log(e);
-});
 
-newUser1.save().then((doc) => {
-  console.log('Saved', doc);
-}, (e) => {
-  console.log(e);
-});
-
-var newTodo = new Todo({
-  text: 'Eat Dinner'
-});
-
-var newTodo1 = new Todo({
-  text: 'Coding',
-  completed: true,
-  completedAt: 123
-});
-
-newTodo.save().then((doc) => {
-  console.log('Saved', doc);
-}, (err) => {
-  console.log('Unable to save', err);
-});
-
-newTodo1.save().then((doc) => {
-  console.log('Saved', doc);
-}, (err) => {
-  console.log('Unable to save', err);
+app.listen(3000, () => {
+  console.log('Server Started on 3000');
 });
