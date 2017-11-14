9d0d95a1801ed7f148a8a19052a9d8b075b3a654
Sri Raju Bushanam Vanteddu <srirajubushanam@gmail.com>
2017-11-13 21:31:17 -0500

    Added JWT to add users to the db

--- server/models/user.js
+++ server/models/user.js
@@ -1,35 +1,63 @@
-var mongoose = require('mongoose');
-var validator = require('validator');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('Users', {
-  userName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{Value} is not a valid email'
-    }
-  },
-  password: {
-    required: true,
-    type: String,
-    minlength: 6
-  },
-  tokens: [{
-    access: {
-
+var UserSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
     },
-    token: {
+    email: {
+      type: String,
+      required: true,
+      minlength: 1,
+      trim: true,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: '{Value} is not a valid email'
+      }
+    },
+    password: {
+      required: true,
+      type: String,
+      minlength: 6
+    },
+    tokens: [{
+      access: {
 
-    }
-  }]
+      },
+      token: {
+
+      }
+    }]
+  }
+);
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.genAuthToken = function() {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({
+    _id: user._id.toHexString(),
+    access
+  }, 'abc123');
+
+  user.tokens.push({access, token});
+
+return user.save().then(() => {
+  return token;
 });
+};
+
+var User = mongoose.model('Users', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
 
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -98,9 +99,12 @@ app.post('/users', (req, res) => {
   console.log(body);
   var user = new User(body);
   user.save().then((user) => {
-    res.send(user);
-  }, (e) => {
-    res.status(400).send();
+    return user.genAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  })
+  .catch((e) => {
+    res.status(400).send(e);
     console.log('Error in Creating user', e);
   });
 });
