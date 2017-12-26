07857fdd9b7a455ca0102034e0dc7b62c499f7a7
rvanteddu <rvanteddu@beckwithelectric.com>
2017-12-26 17:18:26 -0500

    Updated config.js to use config json file

--- server/models/user.js
+++ server/models/user.js
@@ -49,7 +49,7 @@ UserSchema.methods.genAuthToken = function() {
   var token = jwt.sign({
     _id: user._id.toHexString(),
     access
-  }, 'abc123');
+  }, process.env.JWT_SECRET);
 
   user.tokens.push({access, token});
 
@@ -62,7 +62,7 @@ UserSchema.statics.getUserByToken = function(token) {
   var user = this;
   var decoded;
 try{
-  decoded = jwt.verify(token, 'abc123');
+  decoded = jwt.verify(token, process.env.JWT_SECRET);
 }
 catch(e){return Promise.reject();}
 
